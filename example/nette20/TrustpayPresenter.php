e56fcfe0fab2d93c124689ebcb0852c30d9a305c
Kollarovic <kollarovic@gmail.com>
2013-11-24 14:40:13 +0100

    Add example

---
+++ example/nette20/TrustpayPresenter.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Example;
+
+use Kollarovic\TrustPay\TrustPay;
+
+
+class TrustpayPresenter extends BasePresenter
+{
+
+	/** @var \Kollarovic\TrustPay\TrustPay */
+	private $trustPay;
+
+
+	public function injectTrustpay(\Kollarovic\TrustPay\TrustPay $trustPay)
+	{
+		$this->trustPay = $trustPay;
+	}
+
+
+	/**
+	 * @param float
+	 * @param string
+	 * @return void
+	 */
+	public function actionPay($amount, $ref)
+	{
+		$url = $this->trustPay->getPayUrl($amount, $ref);
+		$this->redirectUrl($url);
+	}
+
+
+	/**
+	 * @param float
+	 * @param string
+	 * @return void
+	 */
+	public function actionCardPay($amount, $ref)
+	{
+		$url = $this->trustPay->getCardUrl($amount, $ref);
+		$this->redirectUrl($url);
+	}
+	
+
+	/**
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @return void
+	 */
+	public function actionSuccess($REF, $RES, $PID)
+	{
+		if ($RES === TrustPay::SUCCESS) {
+			$this->flashMessage('Platba prebehla úspešne.');
+		} elseif ($RES === TrustPay::PENDING) {
+			$this->flashMessage('Čakáme na platbu.');
+		//...	
+		} else {
+			$this->flashMessage('Nepodarilo previesť platbu. Prosím, skúste neskôr.', 'warning');
+		}
+		$this->redirect('Homepage:default');
+	}
+
+
+	/**
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @return void
+	 */
+	public function actionError($REF, $RES, $PID)
+	{
+		$this->flashMessage('Nepodarilo previesť platbu. Prosím, skúste neskôr.', 'warning');
+		// save error...
+		$this->redirect('Homepage:default');
+	}
+
+
+	/**
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @return void
+	 */
+	public function actionCancel($REF, $RES, $PID)
+	{
+		$this->flashMessage('Platba bola zrušená zo strany uživateľa.', 'warning');
+		// save cancel...
+		$this->redirect('Homepage:default');
+	}
+
+
+	/**
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @return void
+	 */
+	public function renderNotification($RES, $AMT, $REF)
+	{
+		$result = $this->params;
+		if ($this->trustPay->isOk($result)) { //overime ci neboli podvrhunte data
+			//save result...	
+		}
+	}
+
+}
